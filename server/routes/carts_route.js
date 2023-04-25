07fd4a85af3d342ae9eb01a85a9ce0d138a8b2f3
Barnabas Yuen <barnabas.yuen@sjsu.edu>
2023-04-25 00:11:44 -0700

    carts route

---
+++ server/routes/carts_route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../redisClient')
+
+router.get('/', async (req, res) => {
+    const account_id = req.session.user.account_id;
+    try {
+        const cart = await client.hGetAll(account_id)
+        res.status(200).send(cart);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+router.post('/add', async (req, res) => {
+    const product_id = req.body.product_id;
+    const quantity = req.body.quantity;
+    const account_id = req.session.user.account_id;
+
+    const cartKey = "cart_" + account_id;
+
+    try {
+        await client.hSet(cartKey, product_id, quantity)
+        res.status(200).send('done')
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+module.exports = router;
