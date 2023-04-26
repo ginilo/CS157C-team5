eca7f2363a51a6adc3087ebed78f7451c387b8dc
Barnabas Yuen <barnabas.yuen@sjsu.edu>
2023-04-25 22:23:35 -0700

    fix get cart, remove item from cart

--- server/routes/carts_route.js
+++ server/routes/carts_route.js
@@ -8,8 +8,9 @@ router.get('/', async (req, res) => {
         return;
     }
     const account_id = req.session.user.account_id;
+    const key = "cart_" + account_id 
     try {
-        const cart = await client.hGetAll(account_id)
+        const cart = await client.hGetAll(key)
         res.status(200).send(cart);
     } catch (err) {
         res.status(500).send(err.message)
@@ -29,6 +30,24 @@ router.post('/add', async (req, res) => {
 
     try {
         await client.hSet(cartKey, product_id, quantity)
+        res.status(200).send('done')
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+router.post('/remove', async (req, res) => {
+    if (req.session.user == null) {
+        res.status(401).send("not logged in");
+        return;
+    }
+    const product_id = req.body.product_id;
+    const account_id = req.session.user.account_id;
+
+    const cartKey = "cart_" + account_id;
+
+    try {
+        await client.hDel(cartKey, product_id);
         res.status(200).send('done')
     } catch (err) {
         res.status(500).send(err.message)
